91bfc9d590d5305b5e8dd37f9712b356e365fb92
Mauro <molinamauro2@gmail.com>
2020-08-26 05:01:32 -0300

    add gatsby initial files

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const NotFoundPage = () => (
+  <Layout>
+  </Layout>
+)
+
+export default NotFoundPage
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    <h1>MM Hotel</h1>
+  </Layout>
+)
+
+export default IndexPage

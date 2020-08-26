b5c6f1a754e097a06f12eced59bad4d6a1fda04d
Mauro <molinamauro2@gmail.com>
2020-08-26 10:02:43 -0300

    finish index page

--- src/components/indexContent.js
+++ src/components/indexContent.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { graphql, useStaticQuery} from 'gatsby';
 import Image from 'gatsby-image';
+import styled from '@emotion/styled';
+import {css} from '@emotion/core';
+
+const IndexText = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media (min-width: 768px){
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        column-gap: 2rem;
+    }
+
+    p {
+        line-height: 2;
+    }
+`;
 
 const IndexContent = () => {
 
@@ -25,11 +44,16 @@ const IndexContent = () => {
 
     return ( 
         <>
-            <h2>{title}</h2>
-            <div>
+            <h2
+                css={ css`
+                    text-align: center;
+                    font-size: 4rem;
+                    margin-top: 4rem;
+                `}>{title}</h2>
+            <IndexText>
                 <p>{content}</p>
                 <Image fluid={image.fluid}/>
-            </div>
+            </IndexText>
         </>
      );
 }

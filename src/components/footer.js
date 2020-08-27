5e705c331cb1c5c7c462b80aacc001e513d71342
Mauro <molinamauro2@gmail.com>
2020-08-27 07:20:47 -0300

    add footer

---
+++ src/components/footer.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import Navbar from './navbar';
+import { Link } from 'gatsby'
+
+const HomeLink = styled(Link)`
+    color: #fff;
+    text-align: center;
+    text-decoration: none;
+`;
+
+const Footer = () => {
+
+    const year = new Date().getFullYear();
+
+    return ( 
+        <>
+            <footer
+                css={ css`
+                    background-color: rgba(44, 62, 80);
+                    margin-top: 5rem;
+                    padding: 1rem;
+                `}>
+                <div
+                    css={ css`
+                        max-width: 1200px;
+                        margin: 0 auto;
+
+                        @media (min-width: 768px){
+                            display: flex;
+                            align-items: center;
+                            justify-content: space-between
+                        }
+                    `}>
+                    <Navbar/>
+                    <HomeLink to='/'>
+                        <h1>
+                            MM Hotel
+                        </h1>
+                    </HomeLink>
+                </div>
+            </footer>
+            <p
+                css={ css`
+                    text-align: center;
+                    color: #fff;
+                    background-color: rgb(33,44,55);
+                    margin: 0;
+                    padding: 1rem;
+                `}>
+                MM Hotel. Todos los derechos reservados {year} &copy;
+            </p>
+        </>
+     );
+}
+ 
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { Global, css } from '@emotion/core';  
 import Header from './header';
+import Footer from './footer';
 
 const Layout = (props) => {
     return (
@@ -56,6 +57,7 @@ const Layout = (props) => {
             </Helmet>
             <Header/>
             {props.children}
+            <Footer/>
         </>
      );
 }
